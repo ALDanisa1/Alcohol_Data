9f759715df2fc9d8c4114ec80f4a8ae7ae294f1c
Anele Danisa <aneledanisa10@gmail.com>
2022-07-13 16:19:35 -0800

    Added get_info method

--- src/main.java
+++ src/main.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class main {
@@ -24,7 +25,7 @@ public class main {
         System.out.println("Which country you would like to get alcohol consumption information about? ");
         String country = input.next();
 
-        String country_info = "";
+        List<String> country_info = new ArrayList<>(); ;
         for (int i = 0; i < alcohol_data.size(); i++) {
             if(alcohol_data.get(i).contains(country)){
                country_info = get_info(alcohol_data.get(i));
@@ -32,12 +33,21 @@ public class main {
         }
 
     }
-    public static HashMap<String,String> create_Map(String details){
+    private static HashMap<String,String> create_Map(String details){
         HashMap<String,String> details_map = new HashMap<>();
         String[] types = details.split(",");
         for (int i = 0; i < types.length; i++) {
             details_map.put(types[i],(types[i].substring(0)) );
         }
         return details_map;
+    }
+
+    private static List<String> get_info(String country_info){
+        List<String> info = new ArrayList<>();
+        String[] details = country_info.split(",");
+        for (int i = 0; i < details.length ; i++) {
+            info.add(details[i]);
+        }
+        return info;
     }
 }
